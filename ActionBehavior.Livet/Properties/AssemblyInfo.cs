ad7991e3934bb3bbc55123f54a96a3efc7b71c96
basyura <basyura@gmail.com>
2023-04-29 21:02:04 +0900

    v0.2.0

--- ActionBehavior.Livet/Properties/AssemblyInfo.cs
+++ ActionBehavior.Livet/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("ActionBehavior.Livet")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("ActionBehavior.Livet")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("basyura")]
 [assembly: AssemblyProduct("ActionBehavior.Livet")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
