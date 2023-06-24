27d1f2515e52d1761354ab47edb2edbbf07480bf
basyura <basyura@gmail.com>
2023-06-24 12:15:38 +0900

    Add ViewModel's Extension to invoke command

---
+++ ActionBehavior.Livet/ViewModelExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Livet;
+
+namespace ActionBehavior.Livet
+{
+    public static class ViewModelExtensions
+    {
+        /// <summary>
+        /// Invoke Command.
+        /// </summary>
+        /// <param name="vm"></param>
+        /// <param name="command"></param>
+        /// <param name="parameter"></param>
+        public static void ExecuteCommand(this ViewModel vm, string command, object parameter = null)
+        {
+            var exec = new Execute()
+            {
+                Action = command,
+                ActionParameter = parameter,
+            };
+
+            exec.Invoke(vm);
+        }
+    }
+}
